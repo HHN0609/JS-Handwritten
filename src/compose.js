35b7eff4a1a0d126aba7cf772b5a19a62ddd4eb6
HHN0609 <1446909703@qq.com>
2022-08-04 13:25:11 +0800

    1.修改compose

--- src/compose.js
+++ src/compose.js
@@ -6,10 +6,13 @@
  * @return {Function}
  */
 function compose(...funs){
+    if(funs.length === 0) return (val) => val;
+    if(funs.length === 1) return funs[0];
+
     return function(...args){
         let ans;
         for(let i=funs.length-1; i>=0; i--){
-            if(!ans || i === funs.length-1){
+            if(i === funs.length-1){
                 ans = funs[i](...args);
             } else {
                 ans = funs[i](ans);
@@ -28,3 +31,8 @@ console.log(ans)
 
 let composedFun = compose(fun3, fun2, fun1);
 console.log(composedFun(1));
+
+const a =  () => console.log(1);
+const b =  () => console.log(2);
+const c =  () => console.log(3);
+compose(a, b, c)(); // 分别打印 3 、 2 、 1
